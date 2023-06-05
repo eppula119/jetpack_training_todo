f671b7e3166138df82c27abd018bc777d8ccc779
eppula119 <firstdouga@gmail.com>
2023-06-06 08:58:11 +0900

    Daoの非同期化

--- app/src/main/java/com/example/jetpackbuylistapp/BuyDao.kt
+++ app/src/main/java/com/example/jetpackbuylistapp/BuyDao.kt
@@ -1,18 +1,19 @@
 package com.example.jetpackbuylistapp
 
 import androidx.room.*
+import kotlinx.coroutines.flow.Flow
 
 @Dao // データアクセスオブジェクトであることroomに伝える
 interface BuyDao {
     @Insert // 引数で受け取ったroomデータエンティティをデータベースに保存するためのメソッドということを伝えれる
-    fun insertBuyItem(buyItem: BuyItem) // これだけでroomがデータベースに保存する実装コードを作ってくれる
+    suspend fun insertBuyItem(buyItem: BuyItem) // これだけでroomがデータベースに保存する実装コードを作ってくれる(suspendは、一回だけMainスレッド以外で実行する非同期処理をするという意味)
 
     @Query("SELECT * FROM BuyItem") // SQLの命令文であることを伝える(全ての買う商品を取得)
-    fun loadAllBuyItem(): List<BuyItem> // (返り値は、BuyItem型で配列ですよ。)
+    fun loadAllBuyItem(): Flow<List<BuyItem>> // (返り値は、BuyItem型で配列ですよ。)Flowは、データの変更を監視し変更されば実行される非同期処理を意味する
 
     @Update // 対象のテーブルに保存されたデータを更新することを伝える
-    fun updateBuyItem(buyItem: BuyItem) // buyItemテーブルのデータを更新
+    suspend fun updateBuyItem(buyItem: BuyItem) // buyItemテーブルのデータを更新
 
     @Delete // 削除であることを伝える
-    fun deleteBuyItem(buyItem: BuyItem) // buyItemテーブルのデータを削除
+    suspend fun deleteBuyItem(buyItem: BuyItem) // buyItemテーブルのデータを削除
 }
